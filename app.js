8f43ce9baa72ea9a3cc4f69379a0f11aa5c55d80
akshitha <akshita@vider.in>
2025-07-19 12:03:42 +0530

    laying-foundation

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+
+import React from "react";
+import ReactDOM  from "react-dom/client"
 const heading = React.createElement("h1",{id:"heading"},"Hello World From React")
+console
 // heading is object 
     const root = ReactDOM.createRoot(document.getElementById("root"))
     //render method is responsible for converting obj to h1 tag 
 
-    const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"Im heading"),React.createElement("h2",{},"Im heading")]))
-        root.render(parent);
+    // const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"Im heading"),React.createElement("h2",{},"Im heading")]))
+      const jsxHeading = (<h1 className="heading">This is 
+      rendered in JSX</h1>  )
+
+      const Heading = ()=>(
+        <h1>Hellooooooo</h1>
+      )
+      //component composition
+      const Title =()=>(
+        <>
+        <Heading/>
+        {/* {Heading()} */}
+        <h1>Title</h1>
+
+        </>
+      )
+    root.render(<Title/>);
